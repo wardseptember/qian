2a3bc7c95f74aa21806251e65c9eb2887fd259d6
wardseptember <wardseptember@163.com>
2022-06-16 17:40:38 +0800

    add

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 f = open("./docs/1.md", "w+")
 for filename in os.listdir("./imgs"):
     print(filename)
-    content = """<div align="center"> <img src="../imgs/{}" width="400" height = "600"/> </div><br>
+    content = """![](../imgs/{})
 """.format(filename)
     f.write(content)
 f.close()
